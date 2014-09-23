9b8da8f0cd5a7abde7b344dd60d4c02ed322f21b
Travis Webb <me@traviswebb.com>
2014-09-23 14:29:06 -0400

    setup API

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-google-backbone
-===============
+# google-discovery-backbone
 
-Create Backbone Relational Models from a Google Discovery Document
+[![NPM version][npm-image]][npm-url]
+[![Build status][travis-image]][travis-url]
+[![Dependency Status][daviddm-image]][daviddm-url]
+
+Create a Backbone Relational API from a Google Discovery Document.
+
+## Install
+```sh
+$ npm install google-discovery-backbone --save
+```
+
+## Usage
+```js
+var DiscoveryBackbone = require('google-discovery-backbone');
+var doc = { /* google discovery document */ };
+var models = DiscoveryBackbone.generate(doc);
+```
+
+## License
+MIT
+
+[sails-logo]: http://cdn.tjw.io/images/sails-logo.png
+[sails-url]: https://sailsjs.org
+[npm-image]: https://img.shields.io/npm/v/google-discovery-backbone.svg?style=flat
+[npm-url]: https://npmjs.org/package/google-discovery-backbone
+[travis-image]: https://img.shields.io/travis/tjwebb/google-discovery-backbone.svg?style=flat
+[travis-url]: https://travis-ci.org/tjwebb/google-discovery-backbone
+[daviddm-image]: http://img.shields.io/david/tjwebb/google-discovery-backbone.svg?style=flat
+[daviddm-url]: https://david-dm.org/tjwebb/google-discovery-backbone
---
+++ index.js
@@ -0,0 +1,48 @@
+var WaterlineDiscovery = require('waterline-discovery-doc-import');
+var SailsBackbone = require('sails-backbone');
+var Waterline = require('waterline');
+var Promise = require('bluebird');
+
+/**
+ * Generate a Backbone Relational API from a Google Discovery Document
+ * 
+ * @param doc
+ * @param [config] - optional waterline config object
+ */
+exports.generate = function (doc, config) {
+  var waterline = new Waterline();
+  _.defaults(config, {
+      adapters: {
+        memory: 'sails-memory'
+      },
+      connections: {
+        sailsDiscovery: {
+          adapter: 'memory'
+        }
+      }
+    });
+  var collections = WaterlineDiscovery.createCollections(doc);
+
+  return new Promise(function (resolve, reject) {
+    waterline.initialize(config, function (error, orm) {
+      if (error) return reject(error);
+
+      var sails = createSailsObject(doc, orm);
+      return resolve(SailsBackbone.generate(sails));
+    });
+  });
+};
+
+/**
+ * Manufacture a partially-hydrated sails object for sails-backbone
+ */
+function createSailsObject (doc, orm) {
+  return {
+    models: orm.collections,
+    config: {
+      blueprints: {
+        prefix: doc.servicePath
+      }
+    }
+  };
+}
